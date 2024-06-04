2e88253f0b0a73c545950bd63e021bf67fca5dca
June Gorostidi Elizetxea <96087533+gorosjune@users.noreply.github.com>
2024-06-05 00:55:56 +0200

    Add files via upload

--- sample-creator/Preprocessing.py
+++ sample-creator/Preprocessing.py
@@ -5,20 +5,27 @@ import math
 import scipy.stats as stats
 from sklearn.cluster import KMeans
 from scipy.stats import skew
+from scipy.stats import norm
 import scipy
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 from sklearn.preprocessing import StandardScaler
+import matplotlib
+from itertools import product
+matplotlib.use('TkAgg')  # Use this backend for displaying plots
 
 
-def read_dataframe(file_path):
+# FUNCIONES PRIVADAS
+def read_dataframe(file_path): #En la que me he descargado yo no funciona, para comprobar lo he cargado de otra forma.
     """
     Reads a DataFrame from a CSV file.
 
     Args:
-    - file_path (str): The path to the CSV file.
+    - file_path (str): The path to the CSV file. The CSV file is expected to have the following format:
+      - The first value represents the name.
+      - The following columns represent the variables.
 
     Returns:
     - DataFrame: The DataFrame read from the CSV file.
@@ -29,13 +36,12 @@ def read_dataframe(file_path):
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
         return None
-
-def analyze_df(df):
+def analyze_df(df): 
     """
-    Analyze a DataFrame, remove rows with NaN values in numerical columns, and print the number of NaN values.
+    Analyze a DataFrame, removed NaN values are from numerical columns but retained in categorical columns. Print the number of NaN values.
 
     Args:
-    - df (DataFrame): The input DataFrame.
+    - df (DataFrame): The input DataFrame. 
 
     Returns:
     - DataFrame: The cleaned DataFrame with rows removed if NaN values exist in numerical columns.
@@ -57,30 +63,28 @@ def analyze_df(df):
     
     # Return the original DataFrame if no NaN values exist
     return df
-
-def create_lists_from_df(df):
+def create_variables_dict_from_df(df, columns=None): 
     """
     Create lists of column values from a DataFrame.
 
     Args:
     - df (DataFrame): The input DataFrame.
+    - columns (list): Optional list of columns to include. Default is None (all columns).
 
     Returns:
-    - dict: A dictionary containing column names as keys and lists of column values as values.
+    - dict: A dictionary containing column names as keys and lists of column values as values. 
     """
-    # Dictionary to store lists of column values
     column_lists = {}
     
-    # Iterate over each column in the DataFrame
-    for column_name in df.columns:
-        # Convert column values to a list and store in the dictionary
+    if columns is None:
+        columns = df.columns
+    
+    for column_name in columns:
         column_values = df[column_name].tolist()
         column_lists[column_name] = column_values
     
-    # Return the dictionary
     return column_lists
-
-def dictionary_to_lists(dictionary):
+def dictionary_to_lists(dictionary): #creo que podriamos quitarlo. Esperar hasta ultima comprovacion para borrar
     """
     Convert a dictionary into separate lists for keys and values.
 
@@ -97,13 +101,70 @@ def dictionary_to_lists(dictionary):
     # Return the lists
     return keys, values
 
-#ESTA SIN ACABAR
-def count_elements_in_lists(lists_of_values):
-    distributions = {}
-    for values_list in lists_of_values[1:]:
-        counter = Counter(values_list)
-        distributions[tuple(values_list)] = dict(counter)
-    return distributions
+def separate_numerical_categorical(variables):
+    """
+    Separate numerical and categorical variables from a dictionary containing variables.
+
+    Args:
+    - variables (dict): A dictionary where keys are variable names and values are lists of variable values.
+
+    Returns:
+    - dict: A dictionary containing numerical variables.
+    - dict: A dictionary containing categorical variables.
+    """
+    numerical_variables = {}
+    categorical_variables = {}
+
+    for variable_name, values in variables.items():
+        # Check the type of the first value to determine if the variable is numerical or categorical
+        if isinstance(values[0], (int, float)) and not all(math.isnan(value) or value in (0, 1) for value in values):
+            numerical_variables[variable_name] = values
+        else:
+            categorical_variables[variable_name] = values
+
+    return numerical_variables, categorical_variables
+
+#Solo para categoricas
+def count_elements_in_variables(variables_dict): # Descomentar el salto del name cuando probemos el csv bueno.
+    """
+    Count the occurrences of elements in each list of a variables dictionary, excluding the first variable because is the name, and return a dictionary of Counters.
+
+    Args:
+    - variables_dict (dict): The dictionary of variables where keys are variable names and values are lists of elements. The first variable is excluded from counting.
+
+    Returns:
+    - dict: A dictionary containing Counters for each variable's list of elements.
+    """
+    counters_dict = {}
+    #first = True
+    for variable, values in variables_dict.items():
+        #if first:
+            #first = False
+            #continue
+        counters_dict[variable] = Counter(values)
+    return counters_dict
+def dictionary_to_all_lists(dictionary): 
+    """
+    Convert a dictionary into separate lists of unique keys for each variable.
+
+    Args:
+    - dictionary (dict): The input dictionary.
+
+    Returns:
+    - tuple: A tuple containing two lists of lists: one for keys and one for values.
+    """
+    # Initialize empty lists for keys and values
+    all_keys = []
+    all_values = []
+    
+    # Extract keys and values from each variable's dictionary
+    for variable_dict in dictionary.values():
+        keys = list(variable_dict.keys())
+        values = list(variable_dict.values())
+        all_keys.append(keys)
+        all_values.append(values)
+    
+    return all_keys, all_values
 
 def print_and_collect_statistics(variables):
     """
@@ -123,17 +184,19 @@ def print_and_collect_statistics(variables):
         if i == 0:
             continue  # Skip the first variable
         
+        # Calculate the population size for all variables
+        N = len(values_list)
+        
         # Check if the variable is numerical (assuming the first value represents the type)
         if isinstance(values_list[0], (int, float)):
             # Calculate statistics for numerical variables
-            N = len(values_list)
             mean = np.mean(values_list)
             std_dev = np.std(values_list)
             median = np.median(values_list)
             
             # Store statistics in a dictionary
             stats = {
-                'Population size': N,
+                'Population Size': N,
                 'Mean': mean,
                 'Median': median,
                 'Standard Deviation': std_dev
@@ -141,22 +204,19 @@ def print_and_collect_statistics(variables):
             
             # Print statistics
             print(f"Statistics for numerical variable '{variable_name}':")
-            print(f"  Population size: {N}")
+            print(f"  Population Size: {N}")
             print(f"  Mean: {mean}")
             print(f"  Median: {median}")
             print(f"  Standard Deviation: {std_dev}\n")
         else:
-            # Calculate the number of observations for categorical variables
-            N = len(values_list)
-            
-            # Store the number of observations in a dictionary
+            # Store the number of observations for categorical variables
             stats = {
-                'Number of observations': N
+                'Population Size': N
             }
             
             # Print the number of observations
             print(f"Statistics for categorical variable '{variable_name}':")
-            print(f"  Number of observations: {N}\n")
+            print(f"  Population Size: {N}\n")
         
         # Add the statistics of the current variable to the main dictionary
         stats_dict[variable_name] = stats
@@ -165,13 +225,88 @@ def print_and_collect_statistics(variables):
     return stats_dict
 
 
-# Main Code
-file_path = r"C:\Users\goros\OneDrive\Escritorio\UOC\repository.csv"
-df = read_dataframe(file_path)
-print(df)
+# MAIN FUCTION
+def preprocessing(df):
+    """
+    Preprocesses a CSV file.
+
+    Args:
+    - file_path (str): The path to the CSV file.
+
+    Returns:
+    - dict: A dictionary containing numerical variables.
+    - dict: A dictionary containing categorical variables.
+    """
+    # Read the CSV file
+    #df = pd.read_csv(file_path)
+
+    # Insert a new index - Solo era para mi caso, luego borrar!
+    #df.insert(0, 'New_Index', range(1, len(df) + 1)) 
+    #print(df) 
+
+    # Clean the DataFrame
+    df_clean = analyze_df(df)
+
+    # Create variables dictionary from DataFrame
+    variables = create_variables_dict_from_df(df_clean)
+    keys, values = dictionary_to_lists(variables) #Creo que al final no es necesario. Esperar a la ultima comprobacion para borrar.
+
+    # Separate numerical and categorical variables
+    numerical_variables, categorical_variables = separate_numerical_categorical(variables)
+    # Mostramos las variables numéricas
+    print("Variables numéricas:")
+    for variable_name, values in numerical_variables.items():
+        print(f"{variable_name}: {len(values)}")
+    # Mostramos las variables categóricas
+    print("\nVariables categóricas:")
+    for variable_name, values in categorical_variables.items():
+        print(f"{variable_name}: {len(values)}")
+
+    # Further processing steps for categorical variables
+    counters = count_elements_in_variables(categorical_variables)
+    print("Counter: ", counters)
+    all_keys, all_values = dictionary_to_all_lists(counters)
+    print("keys: ", keys, "values: ", len(values))
+    print("All keys: ", all_keys)
+    print("All values: ", len(all_values))
+
+    # Finally, we compute and print the statistical variables
+    statistics = print_and_collect_statistics(variables)
+
+    return numerical_variables, categorical_variables
+
+
+
+file_path = r"C:\Users\goros\OneDrive\Escritorio\UOC\type_likes.csv" 
+df = pd.read_csv(file_path)
+df.insert(0, 'New_Index', range(1, len(df) + 1)) #Esto solo es para los csv que tengo yo. Borrar desupes.
+print(df) 
+preprocessing(df)
+
+'''
+# MAIN CODE
 df_clean = analyze_df(df)
-variables = create_lists_from_df(df_clean) #diccionario de las variables -> lista de valores
+variables = create_variables_dict_from_df(df_clean)
+
+numerical_variables, categorical_variables = separate_numerical_categorical(variables)
+# Mostramos las variables numéricas
+print("Variables numéricas:")
+for variable_name, values in numerical_variables.items():
+    print(f"{variable_name}: {len(values)}")
+# Mostramos las variables categóricas
+print("\nVariables categóricas:")
+for variable_name, values in categorical_variables.items():
+    print(f"{variable_name}: {len(values)}")
+
+# Para las variables categoricas
+counters = count_elements_in_variables(categorical_variables)
+print("Counter: ", counters)
+
 keys, values = dictionary_to_lists(variables)
+print("keys: ", keys, "values: ", len(values))
+all_keys, all_values = dictionary_to_all_lists(counters)
+print("All keys: ", all_keys)
+print("All values: ", len(all_values))
+
 statistics = print_and_collect_statistics(variables)
-
-
+'''
